84e533abd326449356ae45863612baa00fe3be10
Yekta Süzen <omeryekta@sabanciuniv.edu>
2022-01-16 15:29:23 +0300

    Router and dummy components add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
-
 import "./App.css";
-
 import HamburgerMenu from "./components/HamburgerMenu";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Pages/Home";
-
 import Portfolio from "./components/Pages/Portfolio";
 import Blog from "./components/Pages/Blog";
 import Contact from "./components/Pages/Contact";
