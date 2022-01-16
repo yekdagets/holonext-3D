e19788b4e53688b2469f035b0298d8e3f17507f1
Yekta Süzen <omeryekta@sabanciuniv.edu>
2022-01-16 15:27:59 +0300

    To use html scripts create custom hook use script

--- src/components/useScript.js
+++ src/components/useScript.js
@@ -1,26 +1,3 @@
-// import { useEffect } from "react";
-
-// export default function Scripter(props) {
-
-//     useEffect(() => {
-//       const script = document.createElement('script')
-
-//       Object.assign(script, props)
-  
-//       let { parent='body' } = props
-  
-//       let parentNode = document.querySelector(parent)
-//       parentNode.appendChild(script)
-  
-//       return () => {
-//         parentNode.removeChild(script)
-//       }
-//     } )
-  
-//     return null 
-//   }
-
-
 import { useEffect } from 'react';
 
 const useScript = url => {
